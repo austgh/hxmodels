7c4df4b8984ca27d065433f54402b81b4a7c2734
austgh <46312526+austgh@users.noreply.github.com>
2023-08-21 16:34:49 +0800

    打印数据源信息

--- src/main/java/com/ahzx/mdfc/datasource/DataSourceConfig.java
+++ src/main/java/com/ahzx/mdfc/datasource/DataSourceConfig.java
@@ -3,6 +3,8 @@ package com.ahzx.mdfc.datasource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -16,6 +18,7 @@ import javax.sql.DataSource;
 @Configuration
 //@MapperScan(basePackages = "com.ahzx.mdfc", sqlSessionFactoryRef = "hshySqlSessionFactory")
 public class DataSourceConfig {
+	private static Logger log = LoggerFactory.getLogger(DataSourceConfig.class);
 	@Bean(name = "hshyDataSource")
 	@Primary
 	@ConfigurationProperties(prefix = "spring.datasource")
@@ -27,6 +30,7 @@ public class DataSourceConfig {
 	@Primary
 	public SqlSessionFactory hshySqlSessionFactory(@Qualifier("hshyDataSource") DataSource datasource)
 			throws Exception {
+		log.info("查询到的数据源连接池信息是{}", datasource);
 		SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
 		bean.setDataSource(datasource);
 		bean.setMapperLocations(
